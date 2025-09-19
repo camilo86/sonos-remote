454c33a55837134613d087e40534ef3edf32fc8f
Camilo Gonzalez <cgs.camilogonzalez@gmail.com>
2025-09-19 02:03:51 -0700

    split speakers into two groups to be controlled by 2 pots

--- main.py
+++ main.py
@@ -1,18 +1,23 @@
 from dotenv import load_dotenv
 from soco import discover
-from time import sleep
 from gpiozero import MCP3008
 from utils.volume_control import VolumeControl
 
 
 def main():
     speakers = list(discover())  # type: ignore
-    pot = MCP3008(channel=0)
-    speaker_volume = VolumeControl(pot, speakers)
+    mid = len(speakers) // 2
+    speakers_group_a = speakers[:mid]
+    speakers_group_b = speakers[mid:]
+
+    pot_1 = MCP3008(channel=0)
+    pot_2 = MCP3008(channel=1)
+    speaker_volume_a = VolumeControl(pot_1, speakers_group_a)
+    speaker_volume_b = VolumeControl(pot_2, speakers_group_b)
 
     while True:
-        speaker_volume.control()
-        sleep(1)
+        speaker_volume_a.control()
+        speaker_volume_b.control()
 
 
 def t_volume(value: int):
